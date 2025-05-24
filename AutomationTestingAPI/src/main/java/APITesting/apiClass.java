8f50d5d82ea370c48433dbd03729a13d9bcecf0b
bittumuppu15 <bittumuppu15@gmail.com>
2025-05-25 01:42:49 +0530

    Add files via upload

---
+++ AutomationTestingAPI/src/main/java/APITesting/apiClass.java
@@ -0,0 +1,7 @@
+package APITesting;
+
+public class apiClass {
+public static void main(String[] args) {
+	System.out.println("main.java");
+}
+}
---
+++ AutomationTestingAPI/src/test/java/Authorize/APITestFile.java
@@ -0,0 +1,11 @@
+package Authorize;
+
+public class APITestFile {
+public void APIclas() {
+	Authorize Api=new Authorize();
+	String url="12345";
+	Api.getRequest();
+	System.out.println();
+	
+}
+}
